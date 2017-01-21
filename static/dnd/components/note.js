4ba8f478d2fd4c8b621a20e3509082636ce865a2
min.tang <min.tang@ef.com>
2017-01-21 13:02:27 +0800

    optimize note form

--- static/dnd/components/note.js
+++ static/dnd/components/note.js
@@ -1,6 +1,7 @@
 var m = require("mithril");
-// var Rx = require("rxjs");
 var widget = require("../app");
+var markdown = require("markdown").markdown;
+require("./note.scss");
 
 var note = {
     controller: function (data) {
@@ -8,6 +9,7 @@ var note = {
         vm.data = data;
         vm.note = m.prop(data.task().note || "");
         vm.sending = m.prop(false);
+        
         vm.save = (id, note) => {
             vm.sending(true);
             setTimeout(() => {
@@ -16,31 +18,39 @@ var note = {
             }, 400);
         }
     },
-    view: function (ctrl) {
-        return m(".pomodoro-note-main.ui.modal", [
+    view: function (vm) {
+        return m(".pomodoro-note-main.ui.modal.fullscreen", [
             m(".header", [
-                m("label", `Notes of ${ctrl.data.task().name } ( markdown supported )`),
+                m("p[style='font-weight: normal;']", `Notes of ${vm.data.task().name } ( markdown supported )`),
             ]),
-            m(".content", [
-                m(".ui.form", [
-                    m(".field", [
-                        m("textarea[type='text'][rows='15']#pomodoro-note-main_edit", {
-                            oninput: m.withAttr('value', ctrl.note),
-                            value: ctrl.note(),
-                            onkeypress: function (e) {
-                                if (e.keyCode == 13 && e.ctrlKey) {
-                                    ctrl.save(ctrl.data.task()._id, ctrl.note());
-                                    e.preventDefault(); 
+            m(".content.ui.container", [
+                m(".ui.grid.two.column", [
+                    m(".ui.column.pomodoro-note-main_preview", [
+                        m(".preview", "Preview"),
+                        m(".preview-content", m.trust(markdown.toHTML(vm.note())))
+                    ]),
+                    m(".ui.form.column.pomodoro-note-main_editor", [
+                        m(".field", [
+                            m("textarea[type='text'][rows='25']#pomodoro-note-main_edit", {
+                                oninput: m.withAttr('value', vm.note),
+                                value: vm.note(),
+                                onkeypress: function (e) {
+                                    if (e.keyCode == 13 && e.ctrlKey) {
+                                        vm.save(vm.data.task()._id, vm.note());
+                                        e.preventDefault(); 
+                                    }
                                 }
-                            }
-                        }),
+                            }),
+
+                        ])
                     ])
+
                 ])
             ]),
             m(".actions", [
                 m("button.ui.button.orange.mini.right", {
-                    class: ctrl.sending()? 'loading': '',
-                    onclick: () => ctrl.save(ctrl.data.task()._id, ctrl.note())
+                    class: vm.sending()? 'loading': '',
+                    onclick: () => vm.save(vm.data.task()._id, vm.note())
                 }, "Update (Ctrl + Enter)"),
 
             ])
