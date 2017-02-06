beb8dfb380378f7df6f1a4bdc37a0534720cbceb
gsavl <gc.suprs@gmail.com>
2017-02-06 15:54:16 +0800

    update regex for tag, support unicode tag

--- server/utils/tags.js
+++ server/utils/tags.js
@@ -1,6 +1,6 @@
 function getTags(nameString) {
     if (typeof nameString === 'string') {
-        var tags = nameString.match(/#\w+?#/g) || [];
+        var tags = nameString.match(/#\S+?#/g) || [];
         return tags.map(item => item.replace(/#/g, ''));
     } else {
         throw Error("need string input");
