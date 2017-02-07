6101c25921f4779ed53ae587d990ae0015d485af
tangkikodo <allmonday@126.com>
2017-02-07 10:52:20 +0800

    loose tag restrictions

--- server/utils/tags.js
+++ server/utils/tags.js
@@ -1,7 +1,8 @@
 function getTags(nameString) {
     if (typeof nameString === 'string') {
-        var tags = nameString.match(/#\w+?#/g) || [];
-        return tags.map(item => item.replace(/#/g, ''));
+        // var tags = nameString.match(/#\w+?#/g) || [];
+        var tags = nameString.match(/#.*?#/g) || [];
+        return tags.map(item => item.replace(/#/g, '').trim());
     } else {
         throw Error("need string input");
     }
